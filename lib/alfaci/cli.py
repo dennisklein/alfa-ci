0b76554c0b9636866607926c847ab0fffb7be3bb
Dennis Klein <d.klein@gsi.de>
2020-05-06 00:42:36 +0200

    Initial commit

---
+++ lib/alfaci/cli.py
@@ -0,0 +1,42 @@
+# PYTHON_ARGCOMPLETE_OK
+
+import argcomplete
+import argparse
+
+
+def print_shell_setup(args):
+    """Print the argcomplete bash hook to be eval'ed by the user"""
+    import subprocess
+    print(
+        subprocess.run(['register-python-argcomplete', 'alfa-ci'],
+                       capture_output=True).stdout.decode('UTF-8'))
+
+
+def print_version(args):
+    """Print the package version defined in the setup.py metadata"""
+    from alfaci.version import pkg_version
+    print(pkg_version)
+
+
+def main():
+    """Main entry function called from the CLI"""
+    parser = argparse.ArgumentParser(
+        description='Manage alfa-ci environments.')
+    subparsers = parser.add_subparsers(title='COMMANDS')
+
+    shell_setup_parser = subparsers.add_parser(
+        'shell-setup',
+        add_help=False,
+        help='run \'eval "$(alfa-ci shell-setup)"\' to enable shell completion'
+        ', e.g. from your ~/.bashrc')
+    shell_setup_parser.set_defaults(func=print_shell_setup)
+
+    version_parser = subparsers.add_parser('version',
+                                           add_help=False,
+                                           help='show version number and exit')
+    version_parser.set_defaults(func=print_version)
+
+    argcomplete.autocomplete(parser)
+    args = parser.parse_args()
+
+    args.func(args) if hasattr(args, 'func') else parser.print_help()
---
+++ lib/alfaci/version.py
@@ -0,0 +1,3 @@
+import pkg_resources
+
+pkg_version = pkg_resources.require('alfa-ci')[0].version
